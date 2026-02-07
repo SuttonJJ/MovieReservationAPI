67b9eb44468185755768e9e1100e07a3d49b8fad
SuttonJJ <johannes.sutton2003@gmail.com>
2026-02-07 07:47:57 +0200

    Implemented all auth services and created token request model for refresh tokens

---
+++ MovieReservationAPI/Models/Auth/TokenRequest.cs
@@ -0,0 +1,7 @@
+namespace MovieReservationAPI.Models.Auth;
+
+public class TokenRequest
+{
+    public required string AccessToken { get; set; }
+    public required string RefreshToken { get; set; }
+}
--- MovieReservationAPI/Services/AuthService.cs
+++ MovieReservationAPI/Services/AuthService.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using FluentResults;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
@@ -9,7 +10,7 @@ public interface IAuthService
 {
     Task<Result<AuthResponse>> RegisterAsync(RegisterRequest request);
     Task<Result<AuthResponse>> LoginAsync(LoginRequest request);
-    Task<Result<AuthResponse>> RefreshTokenAsync(string refreshToken);
+    Task<Result<AuthResponse>> RefreshTokenAsync(TokenRequest refreshToken);
 }
 
 public class AuthService(MovieContext context, UserManager<AppUser> userManager, ITokenService tokenService) : IAuthService
@@ -52,11 +53,69 @@ public class AuthService(MovieContext context, UserManager<AppUser> userManager,
 
     public async Task<Result<AuthResponse>> LoginAsync(LoginRequest request)
     {
-        throw new NotImplementedException();
+        var user = await userManager.FindByEmailAsync(request.Email);
+        if (user == null) return Result.Fail<AuthResponse>("Email not found");
+
+        var success = await userManager.CheckPasswordAsync(user, request.Password);
+        if (!success) return Result.Fail<AuthResponse>("Email or password incorrect");
+
+        var accessToken = await tokenService.GenerateJwtToken(user);
+        var refreshToken = tokenService.GenerateRefreshToken();
+
+        user.RefreshToken = refreshToken;
+        user.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(7);
+        await userManager.UpdateAsync(user);
+
+        return Result.Ok(new AuthResponse
+        {
+            AccessToken = accessToken,
+            RefreshToken = refreshToken,
+            Email = user.Email
+        });
     }
 
-    public async Task<Result<AuthResponse>> RefreshTokenAsync(string refreshToken)
+    public async Task<Result<AuthResponse>> RefreshTokenAsync(TokenRequest request)
     {
-        throw new NotImplementedException();
+        if (request == null) return Result.Fail<AuthResponse>("Invalid request");
+
+        string accessToken = request.AccessToken;
+        string refreshToken = request.RefreshToken;
+        
+        ClaimsPrincipal principal;
+        try
+        {
+            principal = tokenService.GetPrincipalFromExpiredToken(accessToken);
+        }
+        catch
+        {
+            return Result.Fail<AuthResponse>("Invalid access token");
+        }
+
+        string? username = principal.Identity?.Name;
+        if (string.IsNullOrEmpty(username))
+        {
+            return Result.Fail<AuthResponse>("Invalid token claims");
+        }
+        var user = await userManager.FindByNameAsync(username);
+        
+        if (user == null || 
+            user.RefreshToken != refreshToken || 
+            user.RefreshTokenExpiryTime <= DateTime.UtcNow)
+        {
+            return Result.Fail<AuthResponse>("Invalid refresh token");
+        }
+
+        var newAccessToken = await tokenService.GenerateJwtToken(user);
+        var newRefreshToken = tokenService.GenerateRefreshToken();
+        
+        user.RefreshToken = newRefreshToken;
+        await userManager.UpdateAsync(user);
+
+        return Result.Ok(new AuthResponse
+        {
+            AccessToken = newAccessToken,
+            RefreshToken = newRefreshToken,
+            Email = user.Email
+        });
     }
 }
